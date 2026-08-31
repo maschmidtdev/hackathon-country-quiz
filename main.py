9404652c5afd72652bc7fac19f1916a5a02d876a
Annika Gilewski <annika.gilewski@gmx.de>
2026-08-31 17:14:27 +0200

    First commit main + 2 txt files

--- main.py
+++ main.py
@@ -1,5 +1,52 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+def function_menu_choice(input_user_choice_menu):
+    if input_user_choice_menu == 1:
+        with open(game_introduction.txt, "r", encoding="utf-8") as file:
+            text = file.read()
+        print(text)
+    elif input_user_choice_menu == 2:
+        if input_user_choice_menu == A:
+            pass
+            # Funktionsaufruf für Start des Spiels
+        elif input_user_choice_menu == B:
+            with open(game_instructions.txt, "r", encoding="utf-8") as file:
+                text_output = file.read()
+            print(text_output)
+        elif input_user_choice_menu == C:
+            pass
+            # Spielmodi (TBC)
+        elif input_user_choice_menu == C:
+            pass
+            # Aufruf Funktion zum Beenden des Spiels
+
+
 def main():
-    pass
+    print("""
+1) Einführung zum Spiel
+2) Wähle folgende Option:
+    A. Neues Spiel
+    B. Spielregeln
+    C. Spielmodi (TBC)
+    D. Spiel beenden
+""")
+
+# Punkt 2) C. mögliche Ergänzung:
+    # Woher kommst du?
+    # Möchtest du das Spiel nur innerhalb eines Kontinentes spielen?
+    # Highscore Liste
 
 if __name__ == '__main__':
     main()
+
