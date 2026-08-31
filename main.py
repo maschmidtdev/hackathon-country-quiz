3e6ce1201aff1c6715efb7077f045b847d1b3edf
Marcel Schmidt <maschmidt.dev@gmail.com>
2026-08-31 17:24:53 +0200

    Implemented partial game logic skeleton

--- main.py
+++ main.py
@@ -1,3 +1,78 @@
+import random
+
+import requests
+from bs4 import BeautifulSoup
+
+
+countries = ["Deutschland", "Frankreich", "Spanien", "Italien", "Namibia"]
+
+info_difficulty = {
+    "schwer": ["Wahlsprache","Amtssprache", "Hauptstadt"],
+    "mittel": ["Staatsoberhaupt","Staatsreligion", "Fläche"],
+    "leicht": ["Zeitzone","Telefonvorwahl", "Kfz-Kennzeichen"]
+}
+
+
+infobox_data = {
+    "Amtssprache": "Französisch"
+}
+
+
+difficulty_counts = {
+    "schwer": 2,
+    "mittel": 3,
+    "leicht": 2
+}
+
+
+def get_info_box(country):
+    url = f"https://de.wikipedia.org/wiki/{country}"
+    response = requests.get(url, headers={"User-Agent": "Quiz"})
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    infobox = soup.find("table", class_="infobox")
+
+    for row in infobox.find_all("tr"):
+        text = row.get_text(separator=": ", strip=True)
+
+
+def get_random_info(difficulty) -> str:
+    # TODO: leere liste abfangen
+    info = random.choice(info_difficulty[difficulty])
+    info_difficulty[difficulty].remove(info)
+
+    if info in infobox_data:
+        return f"{info}: {infobox_data[info]}"
+    else:
+        return get_random_info(difficulty)
+
+
+def get_country_from_user() -> str:
+    guess = input("Auf welches Land tippst du?: ")
+    return guess
+
+
+def play_game(difficulties, country) -> int:
+    score = len(difficulties)
+
+    for difficulty in difficulties:
+        print(get_random_info(difficulty))
+        guess = get_country_from_user()
+
+        if country.lower() == guess.lower():
+            return score
+        else:
+            score -= 1
+
+    return score
+
+
+def get_difficulties() -> list:
+    difficulties = []
+    for key, value in difficulty_counts.items():
+        for i in range(value):
+            difficulties.append(key)
+    return difficulties
 
 
 def game():
@@ -11,6 +86,24 @@ def game():
     #       - Erfolg = gratulieren, punkte display, -> Menü
     #       - Falsch geraten, evtl. schwierigkeit anpassen, nächster Hinweis
     #       - nach X versuchen spiel verloren, menü ?
+
+    # TODO: make function
+    difficulties = get_difficulties()
+    print(difficulties)
+
+    country = random.choice(countries)
+    #info = get_info_box(country)
+    get_info_box(country)
+
+    # Loop
+    score = play_game(difficulties, country)
+
+    if score > 0:
+        # win
+        pass
+    else:
+        # lose
+        pass
 
 
 if __name__ == "__main__":
