fa64c1fc524d47cdd2e17074efed32d603f64db9
Marcel Schmidt <maschmidt.dev@gmail.com>
2026-08-31 16:09:42 +0200

    Comments for development plan

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def main():
-    pass
 
-if __name__ == '__main__':
-    main()
+
+def game():
+    # Menü anzeigen (siehe whiteboard/excalidraw) - Annika
+    # Spielablauf/Spiellogik
+    #   - interne Länder auswahl liste (kontinent auswhal später)
+    #   - infobox daten fetchen - funktion dafür -> Thomas
+    #   - internes dict für schwierigkeiten + infos (schwierigkeit 1, 2 und 3)
+    #   - aus aktueller schwierigkeit random info auswählen + display
+    #   - user input Land
+    #       - Erfolg = gratulieren, punkte display, -> Menü
+    #       - Falsch geraten, evtl. schwierigkeit anpassen, nächster Hinweis
+    #       - nach X versuchen spiel verloren, menü ?
+
+
+if __name__ == "__main__":
+    game()
