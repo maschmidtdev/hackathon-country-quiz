57dd2ac116751e0327d950499b848402f437734e
Marcel Schmidt <maschmidt.dev@gmail.com>
2026-09-01 12:26:01 +0200

    refactoring: helpers and config files

---
+++ config.py
@@ -0,0 +1,51 @@
+info_difficulties = {
+    "ignore": [
+        "Errichtung",
+        "Internet-TLD",
+        "Vorgängergebilde",
+        "ISO 3166",
+        "Nationalfeiertag",
+        "Unabhängigkeit",
+        "Verfassung"
+    ],
+    "maybe": [
+        "National­hymne",
+        "Flagge"
+    ],
+    "schwer": [
+        "Wahlsprache",
+        "Amtssprache",
+        "Telefonvorwahl",
+        "Staats- und Regierungsform",
+        "Bevölkerungsdichte",
+        "Parlament(e)",
+        "Bevölkerungs­entwicklung",
+        "BruttoinlandsproduktTotal (nominal)Total (KKP)BIP/Einw. (nom.)BIP/Einw. (KKP)",
+        "Index der menschlichen Entwicklung(HDI)",
+        "Staatsform",
+        "Bruttoinlandsprodukt",
+        "Brutto­inlands­produkt pro Einwohner",
+        "Bevölkerungs­entwicklung"
+    ],
+    "mittel": [
+        "Staatsoberhaupt",
+        "Staatsreligion",
+        "Fläche",
+        "Regierungschef",
+        "Regierung",
+        "Einwohnerzahl",
+        "Währung"
+    ],
+    "leicht": [
+        "Zeitzone",
+        "Kfz-Kennzeichen",
+        "Hauptstadt",
+        "Nationalhymne"
+    ]
+}
+
+difficulty_counts = {
+    "schwer": 2,
+    "mittel": 3,
+    "leicht": 2
+}
--- main.py
+++ main.py
@@ -1,57 +1,13 @@
 import random
 import json
-from infobox import fetch_infobox_data
+from helpers import get_random_info, get_countries, get_country_from_user, get_difficulties, get_infobox_data
 
 
-with open("info_difficulties.json", "r") as info_file:
-    info_difficulties = json.loads(info_file.read())
-    print(info_difficulties)
-
-infobox_data = {}
-
-difficulty_counts = {
-    "schwer": 2,
-    "mittel": 3,
-    "leicht": 2
-}
-
-def get_countries() -> list:
-    countries = []
-    with open("countries.txt", "r", encoding="utf-8") as country_file:
-        for country in country_file.readlines():
-            countries.append(country.strip())
-
-    return countries
-
-
-def get_random_info(difficulty) -> str:
-    if len(info_difficulties[difficulty]) == 0:
-        return ""
-
-    info = random.choice(info_difficulties[difficulty])
-    info_difficulties[difficulty].remove(info)
-
-    if info in infobox_data:
-        return f"{info}: {infobox_data[info]}"
-    else:
-        return get_random_info(difficulty)
-
-
-def get_country_from_user() -> str:
-    guess = input("Auf welches Land tippst du?: ")
-
-    if not guess:
-        print("Eingabe ungültig, erneute Eingabe:")
-        return get_country_from_user()
-
-    return guess
-
-
-def play_game(difficulties, country) -> int:
+def play_game(difficulties, country, infobox_data) -> int:
     score = len(difficulties)
 
     for difficulty in difficulties:
-        random_info = (get_random_info(difficulty))
+        random_info = (get_random_info(difficulty, infobox_data))
 
         if not random_info:
             score -= 1
@@ -67,26 +23,6 @@ def play_game(difficulties, country) -> int:
 
     return score
 
-
-def get_difficulties() -> list:
-    difficulties = []
-    for key, value in difficulty_counts.items():
-        for i in range(value):
-            difficulties.append(key)
-    return difficulties
-
-
-def print_missing_info(infobox_data, info_difficulty):
-    missing = list(infobox_data.keys())
-    for info in infobox_data:
-        for key ,infolist in info_difficulty.items():
-            if info in infolist:
-                missing.remove(info)
-                break
-
-    print("Missing infos")
-    for info in missing:
-        print("-", info)
 
 def function_menu_choice(input_user_choice_menu):
     if input_user_choice_menu == 1:
@@ -109,27 +45,12 @@ def function_menu_choice(input_user_choice_menu):
             # Aufruf Funktion zum Beenden des Spiels
 
 
-
 def game():
-    # Menü anzeigen (siehe whiteboard/excalidraw) - Annika
-    # Spielablauf/Spiellogik
-    #   - interne Länder auswahl liste (kontinent auswhal später)
-    #   - infobox daten fetchen - funktion dafür -> Thomas
-    #   - internes dict für schwierigkeiten + infos (schwierigkeit 1, 2 und 3)
-    #   - aus aktueller schwierigkeit random info auswählen + display
-    #   - user input Land
-    #       - Erfolg = gratulieren, punkte display, -> Menü
-    #       - Falsch geraten, evtl. schwierigkeit anpassen, nächster Hinweis
-    #       - nach X versuchen spiel verloren, menü ?
-
     difficulties = get_difficulties()
     country = random.choice(get_countries())
-    infobox_data.update(fetch_infobox_data(country))
+    infobox_data = get_infobox_data(country)
 
-    print_missing_info(infobox_data, info_difficulties)
-
-    # Loop
-    score = play_game(difficulties, country)
+    score = play_game(difficulties, country, infobox_data)
 
     if score > 0:
         print("Win! Score:", score)
